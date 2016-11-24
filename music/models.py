4a7488d37f00949c968a0e82b4f56584edf70706
codophobia <shivamm389@gmail.com>
2016-11-25 03:34:04 +0530

    added playlists

--- music/models.py
+++ music/models.py
@@ -1,15 +1,22 @@
 from __future__ import unicode_literals
 from django.db import models
 
-class Playlist(models.Model):
-	genre = models.CharField(max_length = 200)
-
-	def __str__(self):
-		return self.genre
-
 class Song(models.Model):
 	song_title = models.CharField(max_length=250)
 	file = models.FileField(upload_to='/',default = "null")
 
+	class Meta:
+		ordering = ['song_title']
+
 	def __str__(self):
 		return self.song_title
+
+class Playlist(models.Model):
+    name = models.CharField(max_length = 50)
+    songs = models.ManyToManyField(Song)
+
+    def __str__(self):
+        return self.name
+
+
+
--- music/views.py
+++ music/views.py
@@ -1,9 +1,118 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.conf import settings
-from models import Song
+from models import Song,Playlist
+from django.template.loader import render_to_string
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.core import serializers
+import json
+
 def home(request):
 	song = Song.objects.all()
 	context = {"songs":song} 
 	return render(request,'music/base.html',context)
 
+@csrf_exempt
+def playlist(request):
+	playlists = Playlist.objects.all()
+	data = serializers.serialize('json', playlists)
+	return HttpResponse(data, content_type="application/json")
+	
+@csrf_exempt
+def songs(request):
+	song = Song.objects.all()
+	data = serializers.serialize('json', song)
+	return HttpResponse(data, content_type="application/json")
+
+@csrf_exempt
+def next(request):
+	if request.method == 'POST':
+		isplay = int(request.POST['isplay'])
+		currsong = request.POST['currsong']
+		if(isplay == 0):
+			next_song = Song.objects.all().filter(song_title__gt = currsong)[:1]
+			if not next_song:
+				next_song = Song.objects.all()[:1]
+		else:
+			pname = request.POST['pname']
+			p = Playlist.objects.get(name=pname)
+			next_song = p.songs.all().filter(song_title__gt = currsong)[:1]
+			if not next_song:
+				p = Playlist.objects.get(name=pname)
+				next_song = p.songs.all()[:1]
+		data = serializers.serialize('json', next_song)
+		return HttpResponse(data, content_type="application/json")
+
+@csrf_exempt
+def prev(request):
+	if request.method == 'POST':
+		isplay = int(request.POST['isplay'])
+		currsong = request.POST['currsong']
+		if(isplay == 0):
+			next_song = Song.objects.all().filter(song_title__lt = currsong)[:1]
+		else:
+			pname = request.POST['pname']
+			p = Playlist.objects.get(name=pname)
+			next_song = p.songs.all().filter(song_title__lt = currsong)[:1]
+		data = serializers.serialize('json', next_song)
+		return HttpResponse(data, content_type="application/json")
+@csrf_exempt
+def psongs(request):
+	if request.method == 'POST':
+		name = (request.POST['name'])
+		p = Playlist.objects.get(name=name)
+		songs = p.songs.all()
+		data = serializers.serialize('json', songs)
+		return HttpResponse(data, content_type="application/json")
+
+@csrf_exempt
+def createplaylist(request):
+	if request.method == 'POST':
+		name = (request.POST['name'])
+		p = Playlist()
+		p.name = name
+		p.save()
+		return HttpResponse("success")
+
+@csrf_exempt
+def deletesong(request):
+	if request.method == 'POST':
+		song = (request.POST['song'])
+		Song.objects.filter(song_title = song).delete()
+		return HttpResponse("success")
+
+@csrf_exempt
+def deleteplaylistsong(request):
+	if request.method == 'POST':
+		song = (request.POST['song'])
+		playlist = request.POST['playlist']
+		s = Song.objects.get(song_title=song)
+		p = Playlist.objects.get(name=playlist)
+		p.songs.remove(s)
+		return HttpResponse("success")
+
+@csrf_exempt
+def addplaylistsongs(request):
+	if request.method == 'POST':
+		name = (request.POST['name'])
+		songs = Song.objects.all()
+		data = serializers.serialize('json', songs)
+		return HttpResponse(data, content_type="application/json")
+
+@csrf_exempt
+def deleteplaylist(request):
+	if request.method == 'POST':
+		song = (request.POST['playlist'])
+		Playlist.objects.filter(name=song).delete()
+		return HttpResponse("success")
+@csrf_exempt
+def addtoplaylist(request):
+	if request.method == 'POST':
+		name = request.POST['name']
+		songs = request.POST.getlist('songs[]')
+		p = Playlist.objects.get(name=name)
+		for s in songs:
+			s = Song.objects.get(song_title = s)
+			p.songs.add(s)
+		return HttpResponse("success")
